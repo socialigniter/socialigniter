11756ab475ba97ac6fcba35efa8cdc7f78a4ef56
Brennan Novak <brennan@reverseproductions.com>
2010-12-05 16:00:48 -0800

    moved home back into core

---
+++ application/views/dashboard_default/home/index.php
@@ -0,0 +1,8 @@
+<?= $status_updater ?>
+
+<ol id="feed">
+	<?= $timeline_view ?>
+</ol>
+
+<div class="clear"></div>
+
---
+++ application/views/dashboard_default/home/module.php
@@ -0,0 +1,6 @@
+<ol id="feed">
+	<?= $timeline_view ?>
+</ol>
+
+<div class="clear"></div>
+
