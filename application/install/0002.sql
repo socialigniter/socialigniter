793ff1e5e4c7b8f23eafd5c9cd5a6e6fc8dd3ea4
Brennan Novak <brennan@reverseproductions.com>
2010-12-06 20:14:04 -0800

    updated db migration for Oscar

--- application/install/0002.sql
+++ application/install/0002.sql
@@ -1,3 +1,72 @@
-ALTER TABLE  `categories` CHANGE  `category_sub_id`  `parent_id` INT( 11 ) NULL DEFAULT NULL
-ALTER TABLE  `categories` ADD  `type` CHAR( 16 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL AFTER  `module`
-ALTER TABLE  `categories` CHANGE  `module`  `module` CHAR( 16 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE `categories` (
+  `category_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `site_id` int(11) DEFAULT NULL,
+  `permission` char(32) DEFAULT NULL,
+  `module` char(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `type` char(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(128) DEFAULT NULL,
+  `category_url` varchar(128) DEFAULT NULL,
+  `created_at` datetime DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`category_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE `oauth_server_nonce` (
+  `osn_id` int(11) NOT NULL AUTO_INCREMENT,
+  `osn_consumer_key` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `osn_token` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `osn_timestamp` bigint(20) NOT NULL,
+  `osn_nonce` varchar(80) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`osn_id`),
+  UNIQUE KEY `osn_consumer_key` (`osn_consumer_key`,`osn_token`,`osn_timestamp`,`osn_nonce`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=38 ;
+
+
+CREATE TABLE `oauth_server_registry` (
+  `osr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `osr_usa_id_ref` int(11) DEFAULT NULL,
+  `osr_consumer_key` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `osr_consumer_secret` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `osr_enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `osr_status` varchar(16) NOT NULL,
+  `osr_requester_name` varchar(64) NOT NULL,
+  `osr_requester_email` varchar(64) NOT NULL,
+  `osr_callback_uri` varchar(255) NOT NULL,
+  `osr_application_uri` varchar(255) NOT NULL,
+  `osr_application_title` varchar(80) NOT NULL,
+  `osr_application_descr` text NOT NULL,
+  `osr_application_notes` text NOT NULL,
+  `osr_application_type` varchar(20) NOT NULL,
+  `osr_application_commercial` tinyint(1) NOT NULL DEFAULT '0',
+  `osr_issue_date` datetime NOT NULL,
+  `osr_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`osr_id`),
+  UNIQUE KEY `osr_consumer_key` (`osr_consumer_key`),
+  KEY `osr_usa_id_ref` (`osr_usa_id_ref`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+
+CREATE TABLE `oauth_server_token` (
+  `ost_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ost_osr_id_ref` int(11) NOT NULL,
+  `ost_usa_id_ref` int(11) NOT NULL,
+  `ost_token` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ost_token_secret` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ost_token_type` enum('request','access') DEFAULT NULL,
+  `ost_authorized` tinyint(1) NOT NULL DEFAULT '0',
+  `ost_referrer_host` varchar(128) NOT NULL DEFAULT '',
+  `ost_token_ttl` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `ost_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ost_verifier` char(10) DEFAULT NULL,
+  `ost_callback_url` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`ost_id`),
+  UNIQUE KEY `ost_token` (`ost_token`),
+  KEY `ost_osr_id_ref` (`ost_osr_id_ref`),
+  KEY `ost_token_ttl` (`ost_token_ttl`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+ALTER TABLE `oauth_server_token`
+  ADD CONSTRAINT `oauth_server_token_ibfk_1` FOREIGN KEY (`ost_osr_id_ref`) REFERENCES `oauth_server_registry` (`osr_id`) ON DELETE CASCADE ON UPDATE CASCADE;
