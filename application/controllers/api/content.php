e94fa8abcffdc1f3ccad9e70a1f387b9d1c67437
Brennan Novak <brennan@reverseproductions.com>
2010-12-17 10:06:03 -0800

    started on API update

--- application/controllers/api/content.php
+++ application/controllers/api/content.php
@@ -55,6 +55,8 @@ class Content extends Oauth_Controller
 	   	$this->form_validation->set_rules('type', 'Type', 'required');
 	   	$this->form_validation->set_rules('title', 'Title', 'required');	
 	   	$this->form_validation->set_rules('content', 'Content', 'required');
+	   	
+	   	//$this->social_tools->has_access_to_create($this->input->post('type'), $this->oauth_user_id);
 	
 		// Passes Validation
 	    if ($this->form_validation->run() == true)
@@ -115,16 +117,68 @@ class Content extends Oauth_Controller
     /* PUT types */
     function modify_put()
     {
-    	$approve = $this->social_tools->update_comment_approve($this->get('id'));	
+		// Validation Rules
+	   	$this->form_validation->set_rules('module', 'Module', 'required');
+	   	$this->form_validation->set_rules('type', 'Type', 'required');
+	   	$this->form_validation->set_rules('title', 'Title', 'required');	
+	   	$this->form_validation->set_rules('content', 'Content', 'required');
+	   	
+	   	//$this->social_tools->has_access_to_create($this->input->post('type'), $this->oauth_user_id);
+	
+		// Passes Validation
+	    if ($this->form_validation->run() == true)
+	    {	    	
+	    	$viewed			= 'Y';
+	    	$approval		= 'A'; // $this->social_tools->has_access_to_create($this->input->post('type'), $this->oauth_user_id); 
+	   		$status 		= 'P'; //form_submit_publish($this->input->post('publish'), $this->input->post('save_draft'));
+	   		$comments_allow	= $this->input->post('comments_allow');
+	   	
+	    	$content_data = array(				
+				'parent_id'			=> $this->input->post('parent_id'),
+				'category_id'		=> $this->input->post('category_id'),
+				'module'			=> $this->input->post('module'),
+				'type'				=> $this->input->post('type'),
+				'source'			=> $this->input->post('source'),
+				'order'				=> $this->input->post('order'),
+	    		'user_id'			=> $this->oauth_user_id,
+				'title'				=> $this->input->post('title'),
+				'title_url'			=> form_title_url($this->input->post('title'), $this->input->post('title_url')),
+				'content'			=> $this->input->post('content'),
+				'details'			=> $this->input->post('details'),
+				'access'			=> $this->input->post('access'),
+				'comments_allow'	=> $comments_allow,
+				'geo_lat'			=> $this->input->post('geo_lat'),
+				'geo_long'			=> $this->input->post('geo_long'),
+				'geo_accuracy'		=> $this->input->post('geo_accuracy'),
+				'viewed'			=> $viewed,
+				'approval'			=> $approval,				
+				'status'			=> $status  			
+	    	);
+	    									
+			// Insert
+			$content = $this->social_igniter->add_content($content_data, $this->input->post('tags'), '');
+			     		
+		    if ($content)
+		    {	
+				// API Response
+	        	$message	= array('status' => 'success', 'message' => 'Awesome we posted your '.$content_data['type'], 'data' => $content);
+	        	$response	= 200;
+	        }
+	        else
+	        {
+		        $message	= array('status' => 'error', 'message' => 'Oops we were unable to post your '.$content_data['type']);
+		        $response	= 200;		        
+	        }	
+		}
+		else 
+		{
+			// Does Not Pass Validation
+	        $message	= array('status' => 'error', 'message' => validation_errors());
+	        $response	= 200;
+		}
+	
+	    $this->response($message, $response);
 
-        if($approve)
-        {
-            $this->response(array('status' => 'success', 'message' => 'Comment approved'), 200);
-        }
-        else
-        {
-            $this->response(array('status' => 'error', 'message' => 'Could not be approved'), 404);
-        }
     } 
 
     /* DELETE types */
