7da00b1599978acaa858d9dd87befb61323dd8f0
Brennan Novak <brennan@reverseproductions.com>
2011-01-21 18:32:02 -0800

    fixed empty module controller bug

--- application/libraries/Public_Controller.php
+++ application/libraries/Public_Controller.php
@@ -29,36 +29,40 @@ class Public_Controller extends MY_Controller
         $this->data['sidebar']				= $this->load->view(config_item('site_theme').'/partials/sidebar_site.php', $this->data, true);
 		$this->data['footer']				= $this->load->view(config_item('site_theme').'/partials/footer.php', $this->data, true);
 
-		$modules_scan = $this->social_igniter->scan_modules();
+		// If Modules Exist		
+		if ($this->modules_scan)
+		{
+			foreach ($this->modules_scan as $module)
+			{
+				if (config_item($module.'_enabled') == 'TRUE')
+				{			
+					$module_header 		= '/modules/'.$module.'/views/partials/head_site.php';
+					$module_navigation	= '/modules/'.$module.'/views/partials/navigation_site.php';
+					$module_sidebar 	= '/modules/'.$module.'/views/partials/sidebar_site.php';
+					$module_footer 		= '/modules/'.$module.'/views/partials/footer.php';
 		
-		foreach ($modules_scan as $module):
-		
-			$module_header 		= '/modules/'.$module.'/views/partials/head_site.php';
-			$module_navigation	= '/modules/'.$module.'/views/partials/navigation_site.php';
-			$module_sidebar 	= '/modules/'.$module.'/views/partials/sidebar_site.php';
-			$module_footer 		= '/modules/'.$module.'/views/partials/footer.php';
-
-			// Set Module Asset Path
-			$this->data['this_module_assets'] 	= base_url().'application/modules/'.$module.'/assets/';
-		
-		    if (file_exists(APPPATH.$module_header))
-		    {
-		    	$this->data['head'] 		.= $this->load->view('..'.$module_header, $this->data, true);
-		    }
-		    if (file_exists(APPPATH.$module_navigation))
-		    {
-		    	$this->data['navigation'] 	.= $this->load->view('..'.$module_navigation, $this->data, true);
-		    }
-		    if (file_exists(APPPATH.$module_sidebar))
-		    {
-		    	$this->data['sidebar'] 		.= $this->load->view('..'.$module_sidebar, $this->data, true);
-		    }
-		    if (file_exists(APPPATH.$module_footer))
-		    {
-		    	$this->data['footer'] 		.= $this->load->view('..'.$module_footer, $this->data, true);
+					// Set Module Asset Path
+					$this->data['this_module_assets'] 	= base_url().'application/modules/'.$module.'/assets/';
+				
+				    if (file_exists(APPPATH.$module_header))
+				    {
+				    	$this->data['head'] 		.= $this->load->view('..'.$module_header, $this->data, true);
+				    }
+				    if (file_exists(APPPATH.$module_navigation))
+				    {
+				    	$this->data['navigation'] 	.= $this->load->view('..'.$module_navigation, $this->data, true);
+				    }
+				    if (file_exists(APPPATH.$module_sidebar))
+				    {
+				    	$this->data['sidebar'] 		.= $this->load->view('..'.$module_sidebar, $this->data, true);
+				    }
+				    if (file_exists(APPPATH.$module_footer))
+				    {
+				    	$this->data['footer'] 		.= $this->load->view('..'.$module_footer, $this->data, true);
+					}
+				}
 			}
-
-		endforeach;
+		}
     }
 
     function render($layout='site')
