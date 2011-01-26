db175a383f7a32f9d21a1052abc48fe4f12caec7
Brennan Novak <brennan@reverseproductions.com>
2011-01-27 03:03:12 +0800

    saving recent

--- application/core/MY_Input.php
+++ application/core/MY_Input.php
@@ -15,9 +15,14 @@
 */
 class MY_Input extends CI_Input 
 {
+	function __construct()
+	{
+        parent::__construct();  
+    }
+
 	function _sanitize_globals()
 	{
 		$this->_allow_get_array = TRUE;
 		parent::_sanitize_globals();
-	}
+	} 	
 }
