e5560bdd04c0759f0dab94df8bc9c0f61d46d8ae
Brennan Novak <brennan@reverseproductions.com>
2011-01-27 14:37:47 -0800

    added custom config and helper

---
+++ application/helpers/custom_helper.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Custom Helper
+*
+* @package		Social Igniter
+* @subpackage	Feed Helper
+* @link			http://social-igniter.com
+*
+* Description: 	For custom helpers for your application that don't get committed back to core 
+*/
