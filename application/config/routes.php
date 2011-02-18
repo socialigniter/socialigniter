444d077baefd68ca15f404833ac64ebb145c9e80
Brennan Novak <brennan@reverseproductions.com>
2011-02-18 13:35:37 -0800

    fixed bug in routes that was stoping modules with wildcard 2nd URI segments from properly routing

--- application/config/routes.php
+++ application/config/routes.php
@@ -32,47 +32,47 @@
 | would be loaded.
 */
 
-$route['default_controller'] 					= 'site';
-$route['404_override'] 							= 'site/error_404';
+$route['default_controller'] 						= 'site';
+$route['404_override'] 								= 'site/error_404';
 
 /* Feeds */
-$route['feed/(:any)'] 							= '$1/feed';
-$route['feed/comments']							= 'feed/comments';
-$route['feed']		 							= 'feed/index';
+$route['feed/(:any)'] 								= '$1/feed';
+$route['feed/comments']								= 'feed/comments';
+$route['feed']		 								= 'feed/index';
 
 /* Activity */
-$route['api/activity/view/(:any)/(:any)']		= 'api/activity/view/$1/$2';
-$route['api/activity/destroy/(:any)/(:any)']	= 'api/activity/destroy/$1/$2';
-$route['api/activity/recent']					= 'api/activity/recent';
-$route['api/activity/create']					= 'api/activity/create';
+$route['api/activity/view/(:any)/(:any)']			= 'api/activity/view/$1/$2';
+$route['api/activity/destroy/(:any)/(:any)']		= 'api/activity/destroy/$1/$2';
+$route['api/activity/recent']						= 'api/activity/recent';
+$route['api/activity/create']						= 'api/activity/create';
 
 /* Categories */
-$route['api/categories/view/(:any)/(:any)']		= 'api/categories/view';
-$route['api/categories/view']					= 'api/categories/view';
-$route['api/categories/all']					= 'api/categories/all';
-$route['api/categories/create']					= 'api/categories/create';
+$route['api/categories/view/(:any)/(:any)']			= 'api/categories/view';
+$route['api/categories/view']						= 'api/categories/view';
+$route['api/categories/all']						= 'api/categories/all';
+$route['api/categories/create']						= 'api/categories/create';
 
 /* Comments */
-$route['api/comments/content/(:any)/(:any)']	= 'api/comments/content/$1/$2';
-$route['api/comments/viewed/(:any)/(:any)']		= 'api/comments/viewed/$1/$2';
-$route['api/comments/approve/(:any)/(:any)']	= 'api/comments/approve/$1/$2';
-$route['api/comments/destroy/(:any)/(:any)']	= 'api/comments/destroy/$1/$2';
-$route['api/comments/content']					= 'api/comments/content';
-$route['api/comments/recent']					= 'api/comments/recent';
-$route['api/comments/create']					= 'api/comments/create';
-$route['api/comments/public']					= 'api/comments/public';
-$route['api/comments/new']						= 'api/comments/new';
+$route['api/comments/content/(:any)/(:any)']		= 'api/comments/content/$1/$2';
+$route['api/comments/viewed/(:any)/(:any)']			= 'api/comments/viewed/$1/$2';
+$route['api/comments/approve/(:any)/(:any)']		= 'api/comments/approve/$1/$2';
+$route['api/comments/destroy/(:any)/(:any)']		= 'api/comments/destroy/$1/$2';
+$route['api/comments/content']						= 'api/comments/content';
+$route['api/comments/recent']						= 'api/comments/recent';
+$route['api/comments/create']						= 'api/comments/create';
+$route['api/comments/public']						= 'api/comments/public';
+$route['api/comments/new']							= 'api/comments/new';
 
 /* Content */
-$route['api/content/view/(:any)/(:any)']		= 'api/content/view/$1/$2';
-$route['api/content/viewed/(:any)/(:any)']		= 'api/content/viewed/$1/$2';
-$route['api/content/modify/(:any)/(:any)']		= 'api/content/modify/$1/$2';
-$route['api/content/approve/(:any)/(:any)']		= 'api/content/approve/$1/$2';
-$route['api/content/save/(:any)/(:any)']		= 'api/content/save/$1/$2';
-$route['api/content/publish/(:any)/(:any)']		= 'api/content/publish/$1/$2';
-$route['api/content/destroy/(:any)/(:any)']		= 'api/content/destroy/$1/$2';
-$route['api/content/recent']					= 'api/content/recent';
-$route['api/content/create']					= 'api/content/create';
+$route['api/content/view/(:any)/(:any)']			= 'api/content/view/$1/$2';
+$route['api/content/viewed/(:any)/(:any)']			= 'api/content/viewed/$1/$2';
+$route['api/content/modify/(:any)/(:any)']			= 'api/content/modify/$1/$2';
+$route['api/content/approve/(:any)/(:any)']			= 'api/content/approve/$1/$2';
+$route['api/content/save/(:any)/(:any)']			= 'api/content/save/$1/$2';
+$route['api/content/publish/(:any)/(:any)']			= 'api/content/publish/$1/$2';
+$route['api/content/destroy/(:any)/(:any)']			= 'api/content/destroy/$1/$2';
+$route['api/content/recent']						= 'api/content/recent';
+$route['api/content/create']						= 'api/content/create';
 
 /* Relationships */
 $route['api/relationships/follow/(:any)/(:any)']	= 'api/relationships/follow/$1/$2';
@@ -81,92 +81,92 @@ $route['api/relationships/followers/(:any)/(:any)']	= 'api/relationships/followe
 $route['api/relationships/']						= 'api/relationships/';
 
 /* Tags */
-$route['api/tags/all']							= 'api/tags/all';
-$route['api/tags']								= 'api/tags';
+$route['api/tags/all']								= 'api/tags/all';
+$route['api/tags']									= 'api/tags';
 
 /* Users */
-$route['api/users/modify/(:any)/(:any)']		= 'api/users/modify/$1/$2';
-$route['api/users/activate/(:any)/(:any)']		= 'api/users/activate/$1/$2';
-$route['api/users/recent']						= 'api/users/recent';
-$route['api/users/create']						= 'api/users/create';
+$route['api/users/modify/(:any)/(:any)']			= 'api/users/modify/$1/$2';
+$route['api/users/activate/(:any)/(:any)']			= 'api/users/activate/$1/$2';
+$route['api/users/recent']							= 'api/users/recent';
+$route['api/users/create']							= 'api/users/create';
 
 /* API Modules */
-$route['api/(:any)/(:any)/(:any)/(:any)'] 		= '$1/api/$2/$3/$4';
-$route['api/(:any)/(:any)/(:any)'] 				= '$1/api/$2/$3';
-$route['api/(:any)/(:any)'] 					= '$1/api/$2';
-$route['api/(:any)'] 							= 'api/index';
-$route['api/sandbox']							= 'api/sandbox/index';
-$route['api/index']								= 'api/index';
-$route['api']									= 'api/index';
+$route['api/(:any)/(:any)/(:any)/(:any)'] 			= '$1/api/$2/$3/$4';
+$route['api/(:any)/(:any)/(:any)'] 					= '$1/api/$2/$3';
+$route['api/(:any)/(:any)'] 						= '$1/api/$2';
+$route['api/(:any)'] 								= 'api/index';
+$route['api/sandbox']								= 'api/sandbox/index';
+$route['api/index']									= 'api/index';
+$route['api']										= 'api/index';
 
 /* Connections */
-$route['connections/delete/(:num)']				= 'connections/delete';
-$route['connections/delete']					= 'connections/delete';
-$route['connections/(:any)/(:any)/(:any)'] 		= '$1/connections/$2/$3';
-$route['connections/(:any)/(:any)'] 			= '$1/connections/$2';
-$route['connections/(:any)'] 					= '$1/connections/index';
+$route['connections/delete/(:num)']					= 'connections/delete';
+$route['connections/delete']						= 'connections/delete';
+$route['connections/(:any)/(:any)/(:any)'] 			= '$1/connections/$2/$3';
+$route['connections/(:any)/(:any)'] 				= '$1/connections/$2';
+$route['connections/(:any)'] 						= '$1/connections/index';
 
-/* Home Declared */
-$route['home/pages/manage/(:num)']				= 'pages/editor';
-$route['home/pages/create']						= 'pages/editor';
-$route['home/comments/(:any)'] 					= 'home/comments/$1';
-$route['home/comments'] 						= 'home/comments';
-$route['home/friends']							= 'home/index';
-$route['home/likes']							= 'home/index';
-
-/* Home Modules */
-$route['home/(:any)/(:any)/(:any)/(:any)'] 		= '$1/home/$2/$3/$4';
-$route['home/(:any)/(:any)/(:any)'] 			= '$1/home/$2/$3';
-$route['home/(:any)/manage'] 					= 'home/manage/$1';
-$route['home/(:any)/(:any)'] 					= '$1/home/$2';
-$route['home/(:any)'] 							= 'home';
+/* Home */
+$route['home/pages/manage/(:num)']					= 'pages/editor';
+$route['home/pages/create']							= 'pages/editor';
+$route['home/comments/(:any)'] 						= 'home/comments/$1';
+$route['home/comments'] 							= 'home/comments';
+$route['home/friends']								= 'home/index';
+$route['home/likes']								= 'home/index';
 
 /* Home Partials */
-$route['home/item_timeline']					= 'home/item_timeline';
-$route['home/item_manage']						= 'home/item_manage';
-$route['home/category_editor']					= 'home/category_editor';
-$route['home'] 									= 'home';
+$route['home/item_timeline']						= 'home/item_timeline';
+$route['home/item_manage']							= 'home/item_manage';
+$route['home/category_editor']						= 'home/category_editor';
+$route['home'] 										= 'home';
+
+/* Home Modules */
+$route['home/(:any)/(:any)/(:any)/(:any)'] 			= '$1/home/$2/$3/$4';
+$route['home/(:any)/(:any)/(:any)'] 				= '$1/home/$2/$3';
+$route['home/(:any)/manage'] 						= 'home/manage/$1';
+$route['home/(:any)/(:any)'] 						= '$1/home/$2';
+$route['home/(:any)'] 								= 'home';
 
 /* Profiles */
-$route['profile/:any/image'] 					= 'profile/image';
-$route['profile/:any'] 							= 'profile/index';
+$route['profile/:any/image'] 						= 'profile/image';
+$route['profile/:any'] 								= 'profile/index';
 
 /* Settings */
-$route['settings/(:any)'] 						= '$1/settings/index';
-$route['settings/profile'] 						= 'settings/profile';
-$route['settings/account'] 						= 'settings/account';
-$route['settings/password'] 					= 'settings/password';
-$route['settings/mobile'] 						= 'settings/mobile';
-$route['settings/connections'] 					= 'settings/connections';
-$route['settings/site']							= 'settings/site';
-$route['settings/themes']						= 'settings/themes';
-$route['settings/widgets']						= 'settings/widgets';
-$route['settings/services']						= 'settings/services';
-$route['settings/modules'] 						= 'settings/modules';
-$route['settings/home']							= 'settings/home';
-$route['settings/comments']						= 'settings/comments';
-$route['settings/api']							= 'settings/api';
-$route['settings/update']						= 'settings/update';
-$route['settings/users']						= 'settings/users';
-$route['settings/pages']						= 'settings/pages';
-$route['settings'] 								= 'settings';
+$route['settings/profile'] 							= 'settings/profile';
+$route['settings/account'] 							= 'settings/account';
+$route['settings/password'] 						= 'settings/password';
+$route['settings/mobile'] 							= 'settings/mobile';
+$route['settings/connections'] 						= 'settings/connections';
+$route['settings/site']								= 'settings/site';
+$route['settings/themes']							= 'settings/themes';
+$route['settings/widgets']							= 'settings/widgets';
+$route['settings/services']							= 'settings/services';
+$route['settings/modules'] 							= 'settings/modules';
+$route['settings/home']								= 'settings/home';
+$route['settings/comments']							= 'settings/comments';
+$route['settings/api']								= 'settings/api';
+$route['settings/update']							= 'settings/update';
+$route['settings/users']							= 'settings/users';
+$route['settings/pages']							= 'settings/pages';
+$route['settings/(:any)'] 							= '$1/settings';
+$route['settings'] 									= 'settings';
 
-/* Users Dashboard */
-$route['users/manage/(:any)']					= 'users/editor';
-$route['users/create']							= 'users/editor';
+/* Users Admin */
+$route['users/manage/(:any)']						= 'users/editor';
+$route['users/create']								= 'users/editor';
 
-/* Public Site */
-$route['login']									= 'site/login';
-$route['logout']								= 'site/logout';
-$route['signup']								= 'site/signup';
-$route['forgot_password']						= 'site/forgot_password';
-$route['reset_password/(:any)']					= 'site/reset_password';
-$route['reset_password']						= 'site/reset_password';
-$route['activate/(:any)/(:any)']				= 'site/activate';
-$route['activate/(:any)']						= 'site/activate';
-$route['activate']								= 'site/activate';
-$route['pages/(:any)']							= 'site/pages';
-$route['index']									= 'site/index';
+/* Site */
+$route['login']										= 'site/login';
+$route['logout']									= 'site/logout';
+$route['signup']									= 'site/signup';
+$route['forgot_password']							= 'site/forgot_password';
+$route['reset_password/(:any)']						= 'site/reset_password';
+$route['reset_password']							= 'site/reset_password';
+$route['activate/(:any)/(:any)']					= 'site/activate';
+$route['activate/(:any)']							= 'site/activate';
+$route['activate']									= 'site/activate';
+$route['pages/(:any)']								= 'site/pages';
+$route['index']										= 'site/index';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
