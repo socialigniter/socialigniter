b2a27846d171d0e14139ec813122a7e8f0cc29e6
Tyler Gillies <tjgillies@gmail.com>
2011-04-07 04:43:48 -0700

    added hcard line to webfinger template

--- application/views/site_default/partials/webfinger_user.php
+++ application/views/site_default/partials/webfinger_user.php
@@ -3,4 +3,6 @@
 <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
 	<Subject><?= $uri ?></Subject>
 	<Alias><?= base_url() ?>profile/<?= $username ?></Alias>
+	<Link rel='http://microformats.org/profile/hcard'
+    	href='<?= base_url() ?>profile/<?= $username ?>' />
 </XRD>
