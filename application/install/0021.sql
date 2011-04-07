77af4e8c01137a79e31a0098734a57a5b5424ed1
Brennan Novak <brennan@reverseproductions.com>
2011-04-06 19:43:30 -0700

    added crud permissions to DB

---
+++ application/install/0021.sql
@@ -0,0 +1,3 @@
+INSERT INTO `settings` VALUES(NULL, 1, 'pages', 'crud_permission', '2');
+INSERT INTO `settings` VALUES(NULL, 1, 'places', 'crud_permission', '2');
+INSERT INTO `settings` VALUES(NULL, 1, 'users', 'crud_permission', '2');
