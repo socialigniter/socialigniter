9a835d7f5c48f4187705dee1716c87ed9410f34d
Tyler Gillies <tjgillies@gmail.com>
2011-04-07 15:57:53 -0700

    pubsubhubbub

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -56,66 +56,78 @@ class Profile extends Site_Controller
 	 		
 
 			// Sidebar
-			$this->data['sidebar_profile'] = $this->load->view(config_item('site_theme').'/partials/sidebar_profile.php', $this->data, true);			
+			$this->data['sidebar_profile'] = $this->load->view(config_item('site_theme').'/partials/sidebar_profile.php', $this->data, true);	
+			
+			
+			// Timeline 		
+			$timeline 							= $this->social_igniter->get_timeline_user($this->user->user_id, 8);
+			$timeline_view 						= NULL;	
+			$timeline_count						= 1;
+			$this->data['activities']			= array();
+			$this->data['social_igniter']		= $this->social_igniter;
+					 			
+			if (!empty($timeline))
+			{
+				foreach ($timeline as $activity)
+				{	
+					$timeline_count++;
+						
+					// Item
+					array_push($this->data['activities'], $activity);
+					$this->data['item_id']				= $activity->activity_id;
+					$this->data['item_type']			= item_type_class($activity->type);
+					// Contributor
+					$this->data['item_user_id']			= $activity->user_id;
+					$this->data['item_avatar']			= $this->social_igniter->profile_image($activity->user_id, $activity->image, $activity->gravatar);
+					$this->data['item_contributor']		= $activity->name;
+					$this->data['item_profile']			= base_url().'profile/'.$activity->username;
+					
+					// Activity
+					$this->data['item_content']			= $this->social_igniter->render_item($activity);
+					$this->data['item_content_id']		= $activity->content_id;
+					$this->data['item_date']			= format_datetime(config_item('home_date_style'), $activity->created_at);			
+
+			 		// Actions
+				 	$this->data['item_comment']			= base_url().'comment/item/'.$activity->activity_id;
+				 	$this->data['item_comment_avatar']	= $this->data['logged_image'];
+
+				 	$this->data['item_can_modify']		= $this->social_tools->has_access_to_modify('activity', $activity, $this->session->userdata('user_id'), $this->session->userdata('user_level_id'));			 	
+					$this->data['item_edit']			= base_url().'home/'.$activity->module.'/manage/'.$activity->content_id;
+					$this->data['item_delete']			= base_url().'status/delete/'.$activity->activity_id;
+
+					// View
+					$timeline_view  .= $this->load->view(config_item('site_theme').'/partials/user_timeline.php', $this->data, true);
+		 		}
+		 	}
+		 	else
+		 	{
+		 		$timeline_view = '<li>Nothing to show from anyone!</li>';
+	 		}		
 		}
 		else
 		{
 			redirect(404);
 		}
+		$this->data['timeline_view'] 	= $timeline_view; 		
+ 		$this->data['timeline_count']	= $timeline_count;
+
     }
 
  	function index()
  	{ 		
  		$this->data['page_title'] = $this->data['name']."'s profile";
  		
-		// Timeline 		
-		$timeline 							= $this->social_igniter->get_timeline_user($this->user->user_id, 8);
-		$timeline_view 						= NULL;	
-		$timeline_count						= 1;	
-				 			
-		if (!empty($timeline))
-		{
-			foreach ($timeline as $activity)
-			{	
-				$timeline_count++;
-					
-				// Item
-				$this->data['item_id']				= $activity->activity_id;
-				$this->data['item_type']			= item_type_class($activity->type);
-				
-				// Contributor
-				$this->data['item_user_id']			= $activity->user_id;
-				$this->data['item_avatar']			= $this->social_igniter->profile_image($activity->user_id, $activity->image, $activity->gravatar);
-				$this->data['item_contributor']		= $activity->name;
-				$this->data['item_profile']			= base_url().'profile/'.$activity->username;
-				
-				// Activity
-				$this->data['item_content']			= $this->social_igniter->render_item($activity);
-				$this->data['item_content_id']		= $activity->content_id;
-				$this->data['item_date']			= format_datetime(config_item('home_date_style'), $activity->created_at);			
 
-		 		// Actions
-			 	$this->data['item_comment']			= base_url().'comment/item/'.$activity->activity_id;
-			 	$this->data['item_comment_avatar']	= $this->data['logged_image'];
-
-			 	$this->data['item_can_modify']		= $this->social_tools->has_access_to_modify('activity', $activity, $this->session->userdata('user_id'), $this->session->userdata('user_level_id'));			 	
-				$this->data['item_edit']			= base_url().'home/'.$activity->module.'/manage/'.$activity->content_id;
-				$this->data['item_delete']			= base_url().'status/delete/'.$activity->activity_id;
-
-				// View
-				$timeline_view .= $this->load->view(config_item('site_theme').'/partials/user_timeline.php', $this->data, true);
-	 		}
-	 	}
-	 	else
-	 	{
-	 		$timeline_view = '<li>Nothing to show from anyone!</li>';
- 		}
 
 		// Final Output
-		$this->data['timeline_view'] 	= $timeline_view; 		
- 		$this->data['timeline_count']	= $timeline_count;
+
  		
 		$this->render('profile');
+ 	}
+
+ 	function feed()
+ 	{
+ 		$this->load->view('site_default/partials/feed', $this->data);
  	}
  	
  	function image()
--- application/views/site_default/partials/webfinger_user.php
+++ application/views/site_default/partials/webfinger_user.php
@@ -5,4 +5,9 @@
 	<Alias><?= base_url() ?>profile/<?= $username ?></Alias>
 	<Link rel='http://microformats.org/profile/hcard'
     	href='<?= base_url() ?>profile/<?= $username ?>' />
+    <Link rel='http://schemas.google.com/g/2010#updates-from'
+  		type='application/atom+xml'
+  		href='<?= base_url() ?>profile/<?= $username ?>/feed'
+  	/>
+
 </XRD>
