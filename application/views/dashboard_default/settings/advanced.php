8a08e7069cc5ab560b7571efdb8d497fe43c2fb7
Brennan Novak <brennan@reverseproductions.com>
2011-04-07 16:59:41 -0700

    added advanced page for user edit

---
+++ application/views/dashboard_default/settings/advanced.php
@@ -0,0 +1,13 @@
+<h3>Notifications</h3>
+
+<p>You will be able to update your notification settings for various types of activity</p>
+
+<p>Comments</p>
+
+<p>Messages</p>
+
+<p>Friends</p>
+
+<h3>Webfinger</h3>
+
+<p>Coming soon to an user settings page near you- custom editing of your webfinger records</p>
