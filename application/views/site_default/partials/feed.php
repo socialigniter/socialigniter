176bb2786f00798a519e2549c4687234a39881f7
Tyler Gillies <tjgillies@gmail.com>
2011-04-08 14:15:42 +0800

    should validate now, need to edit updated and id

--- application/views/site_default/partials/feed.php
+++ application/views/site_default/partials/feed.php
@@ -1,6 +1,7 @@
 <?= '<'?>?xml version="1.0" encoding="UTF-8"?<?= '>' ?>
 
 <feed xml:lang="en-US" xmlns="http://www.w3.org/2005/Atom" xmlns:thr="http://purl.org/syndication/thread/1.0" xmlns:georss="http://www.georss.org/georss" xmlns:activity="http://activitystrea.ms/spec/1.0/" xmlns:media="http://purl.org/syndication/atommedia" xmlns:poco="http://portablecontacts.net/spec/1.0" xmlns:ostatus="http://ostatus.org/schema/1.0" xmlns:statusnet="http://status.net/schema/api/1/">
+	<updated><?= date('Y-m-d\TH:i:s\Z', strtotime($activities[0]->created_at)) ?></updated>
 	<id><?= base_url() ?>profile/<?= $username ?>/feed</id>
 	<title><?= $username ?>'s profile</title>
 	<author>
@@ -13,10 +14,10 @@
 	<link href='<?= base_url() ?>profile/<?= $username ?>'/>
 	<? foreach($activities as $activity): ?>
 		<? if ($activity->type == "status"): ?>
-			<? $item_date = format_datetime(config_item('home_date_style'), $activity->created_at); ?>	
 		    <entry>
+		    	<id>http://damn.this.spec.is.hard</id>
 				<title>Status Update</title> 
-				<link href='#TODO add link'/>
+				<link href='http://need.to.add.com'/>
 				<summary><?= $social_igniter->render_item($activity); ?></summary>
 				<updated><?= date('Y-m-d\TH:i:s\Z', strtotime($activity->created_at)) ?></updated>
 
