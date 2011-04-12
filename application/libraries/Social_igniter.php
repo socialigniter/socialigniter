10d232f332bd23af3ed775eb85b2cddd2fee1cf7
Brennan Novak <brennan@reverseproductions.com>
2011-04-11 19:25:22 -0700

    trying to fix Tylers pubsubhubub

--- application/libraries/Social_igniter.php
+++ application/libraries/Social_igniter.php
@@ -624,14 +624,19 @@ class Social_igniter
 	{
 		if ($activity_id = $this->ci->activity_model->add_activity($activity_info, $activity_data))
 		{
-			$username = $this->ci->activity_model->get_activity($activity_id)->username;
-			$hub = 'http://pubsubhubbub.appspot.com/';
-			$hubargs = array('hub.mode'=>'publish', 'hub.url' => base_url() . "profile/". $username.'/feed');
-			$this->ci->load->library('curl');
+			$activity = $this->ci->activity_model->get_activity($activity_id);
+		
+			if ($activity)
+			{
+				$username	= $activity->username;
+				$hub		= 'http://pubsubhubbub.appspot.com/';
+				$hubargs	= array('hub.mode'=>'publish', 'hub.url' => base_url() . "profile/". $username.'/feed');
 			
-			$this->ci->curl->simple_post($hub, $hubargs);
-			//$this->ci->curl->simple_post('http://social.pdxbrain.com:3232', $hubargs);
-			return $this->ci->activity_model->get_activity($activity_id);
+				$this->ci->load->library('curl');	
+				$this->ci->curl->simple_post($hub, $hubargs);
+			}
+
+			return $activity;
 		}
 		
 		return FALSE;
