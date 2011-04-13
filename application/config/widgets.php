fd01c3ef447adfd7fd6c1bef4fa3457bab084a4f
Brennan Novak <brennan@reverseproductions.com>
2011-04-12 20:24:43 -0700

    fixed widgets editor bug

--- application/config/widgets.php
+++ application/config/widgets.php
@@ -1,4 +1,4 @@
-i<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 /**
 * Name:			Social Igniter : Core : Widgets
 * Author: 		Brennan Novak
--- application/views/dashboard_default/partials/google_analytics.php
+++ application/views/dashboard_default/partials/google_analytics.php
@@ -1,4 +1,5 @@
 <script type="text/javascript">
+
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', '<?= config_item('services_google_analytics') ?>']);
   _gaq.push(['_trackPageview']);
@@ -8,4 +9,5 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
 </script>
