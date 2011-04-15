da84899d67d41399596882dfc1a5ae5795596010
Brennan Novak <brennan@reverseproductions.com>
2011-04-14 21:48:35 -0700

    fixed mobile theme

--- application/views/mobile_default/layouts/site.php
+++ application/views/mobile_default/layouts/site.php
@@ -29,9 +29,6 @@
 			<div id="content_message" class="message_normal"><?= $message ?></div>
 			<?= $content ?>
 		</div>
-		<div id="sidebar">
-			<?= $sidebar ?>
-		</div>
 	</div>
 	<div class="clear"></div>
 
@@ -43,7 +40,6 @@
 	</div>
 	<div class="clear"></div>
 </div>
-
-<?= $settings['services']['google_analytics'] ?>
+<?= $google_analytics ?>
 </body>
 </html>
