b0b8a4d065f82d5541ad4c1d451c8163251ea4d8
Tyler Gillies <tjgillies@gmail.com>
2011-04-23 14:35:07 +0800

    needed vcard class

--- application/views/site_default/profile/index.php
+++ application/views/site_default/profile/index.php
@@ -2,7 +2,7 @@
 	<img src="<?= $this->social_igniter->profile_image($user_id, $image, $gravatar, 'large'); ?>" border="0">
 </div>
 
-<div id="profile_info">
+<div id="profile_info" class="vcard">
 	<ul>
 		<li><h1><span class="fn"><?= $name ?></span></h1></li>
 		<li><h3>@<?= $username ?></h3></li>
