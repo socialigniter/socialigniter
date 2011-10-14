ebf58e8a6ddbfb39f0ce9d19484c2f0d5c438c65
Brennan Novak <brennan@reverseproductions.com>
2011-10-13 19:59:29 -0700

    updated validator plugin

--- js/social.core.js
+++ js/social.core.js
@@ -1060,6 +1060,17 @@ function utf8_encode(argString) {
     return utftext;
 }
 
+function isInt(value)
+{ 
+	var y=parseInt(value); 
+	if (isNaN(y))
+	{
+		return false;
+	}
+	
+	return value==y && value.toString()==y.toString(); 
+}
+
 /* md5() @returns {string} a MD5 hash of the string you give it @requires utf8_encode() */
 function md5(str) {
     var xl;
@@ -1984,7 +1995,7 @@ $(function(){
 	Awesome form validation and messaging plugin
 	Settings are:
 	 - element	: Array of elements, contains: selector, rule, field, action (label)
-	 - action	: Action handles failure to validate options are: callback, alert, label
+	 - styles	: Styles for labels and input fields
 	 - message	: Is appended to the start of invalid elements 'Please enter a _________'
 */
 (function($)
@@ -1994,7 +2005,7 @@ $(function(){
 		var defaults = 
 		{
 			elements	: [],
-			action		: '',
+			styles		: { valid : 'form_valid', error : 'form_error' },
 			message		: '',
 			success		: function(){},
 			failed		: function(error_messages){}
@@ -2006,15 +2017,26 @@ $(function(){
 		var element_count	= settings.elements.length;
 		var error_messages	= '';
 
-
 		// Validate Rules
 		function validateRequire(value)
-		{
+		{		
 			if (value != '')
-			{
+			{			
 				return true;
 			}
 			
+			return false;
+		}
+
+		function validateInteger(value)
+		{		
+			if (value > 0)
+			{
+				console.log('inside here ' + value);
+				return true;
+			}
+
+			console.log('over here');			
 			return false;
 		}
 
@@ -2042,6 +2064,7 @@ $(function(){
 			if (valid && $(selector_error).length != 0)
 			{
 				$(element.selector + '_error').html('').addClass(settings.styles.valid);			
+				$(element.selector + '_error').oneTime(300, function() { $(this).fadeOut() });
 			}
 			else
 			{			
@@ -2084,6 +2107,10 @@ $(function(){
 			if (element.rule == 'require')
 			{
 				is_valid = validateRequire(validate);				
+			}
+			else if (element.rule == 'require_integer')
+			{
+				is_valid = validateInteger(validate);				
 			}
 			else if (element.rule == 'email')
 			{
@@ -2544,70 +2571,3 @@ var autocomplete = function(trigger_element, api_data, field, callback)
  	   };
 	} 
 }
-
-
-/*	popupWindow - jQuery Plugin
-	
-*/
-(function($){ 		  
-	$.fn.popupWindow = function(instanceSettings){
-		
-		return this.each(function(){
-		
-		$(this).click(function(){
-		
-		$.fn.popupWindow.defaultSettings = {
-			centerBrowser:0, // center window over browser window? {1 (YES) or 0 (NO)}. overrides top and left
-			centerScreen:0, // center window over entire screen? {1 (YES) or 0 (NO)}. overrides top and left
-			height:500, // sets the height in pixels of the window.
-			left:0, // left position when the window appears.
-			location:0, // determines whether the address bar is displayed {1 (YES) or 0 (NO)}.
-			menubar:0, // determines whether the menu bar is displayed {1 (YES) or 0 (NO)}.
-			resizable:0, // whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.
-			scrollbars:0, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
-			status:0, // whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}.
-			width:500, // sets the width in pixels of the window.
-			windowName:null, // name of window set from the name attribute of the element that invokes the click
-			windowURL:null, // url used for the popup
-			top:0, // top position when the window appears.
-			toolbar:0 // determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}.
-		};
-		
-		settings = $.extend({}, $.fn.popupWindow.defaultSettings, instanceSettings || {});
-		
-		var windowFeatures =    'height=' + settings.height +
-								',width=' + settings.width +
-								',toolbar=' + settings.toolbar +
-								',scrollbars=' + settings.scrollbars +
-								',status=' + settings.status + 
-								',resizable=' + settings.resizable +
-								',location=' + settings.location +
-								',menuBar=' + settings.menubar;
-
-				settings.windowName = this.name || settings.windowName;
-				settings.windowURL = this.href || settings.windowURL;
-				var centeredY,centeredX;
-			
-				if(settings.centerBrowser){
-						
-					if ($.browser.msie) {//hacked together for IE browsers
-						centeredY = (window.screenTop - 120) + ((((document.documentElement.clientHeight + 120)/2) - (settings.height/2)));
-						centeredX = window.screenLeft + ((((document.body.offsetWidth + 20)/2) - (settings.width/2)));
-					}else{
-						centeredY = window.screenY + (((window.outerHeight/2) - (settings.height/2)));
-						centeredX = window.screenX + (((window.outerWidth/2) - (settings.width/2)));
-					}
-					window.open(settings.windowURL, settings.windowName, windowFeatures+',left=' + centeredX +',top=' + centeredY).focus();
-				}else if(settings.centerScreen){
-					centeredY = (screen.height - settings.height)/2;
-					centeredX = (screen.width - settings.width)/2;
-					window.open(settings.windowURL, settings.windowName, windowFeatures+',left=' + centeredX +',top=' + centeredY).focus();
-				}else{
-					window.open(settings.windowURL, settings.windowName, windowFeatures+',left=' + settings.left +',top=' + settings.top).focus();	
-				}
-				return false;
-			});
-			
-		});	
-	};
-})(jQuery);
