f30773b6d2ccea90b1798840a58276cc02be3002
Brennan Novak <brennan@reverseproductions.com>
2011-10-26 22:50:44 -0700

    updated forgot_password view

--- application/views/site_default/site/forgot_password.php
+++ application/views/site_default/site/forgot_password.php
@@ -2,56 +2,53 @@
 <p>Please enter your email address</p>
 
 <form method="post" name="forgot_password" id="forgot_password" action="<?= base_url()."api/users/password_forgot"; ?>">
-	<p><input type="text" name="email" value=""></p>
+	<p>
+		<input type="text" name="email" id="forgot_email" value="">
+	  	<span id="forgot_email_error"></span>
+	</p>
 	<p id="email_error"></p>
 	<p><input type="submit" name="submit" value="Retrieve" /></p>
 </form>
 
 <script type="text/javascript">
-// Elements for Placeholder
-var validation_rules = [{
-	'element' 	: '[name=email]', 
-	'holder'	: 'your@email.com', 
-	'message'	: 'Enter your email'	
-}]
-
 $(document).ready(function()
-{
-	// Placeholders
-	makePlaceholders(validation_rules);
-
+{			
 	$("#forgot_password").bind('submit', function(e)
 	{
-		e.preventDefault();
-		var email_valid	= validateEmailAddress($('[name=email]').val());
+		e.preventDefault();	
+		$.validator(
+		{
+			elements :		
+				[{
+				'selector' 	: '#forgot_email', 
+				'rule'		: 'email', 
+				'field'		: 'Please enter a valid email',
+				'action'	: 'label'	
+				}],
+			message : '',
+			success	: function()
+			{					
 
-		// Validation
-		if (validationRules(validation_rules) && email_valid == true)
-		{
-			$.ajax(
-			{
-				url			: base_url + 'api/users/password_forgot',
-				type		: 'POST',
-				dataType	: 'json',
-				data		: $('#forgot_password').serializeArray(),
-		  		success		: function(result)
-		  		{					
-					if (result.status == 'success')
-					{
-						$('#content_message').notify({status:result.status,message:result.message,complete:'redirect',redirect: base_url + 'login'});					
-					}
-					else
-					{
-						$('#content_message').notify({status:result.status,message:result.message});					
-					}
-			 	}
-			});
-		}
-		else if (validationRules(validation_rules) && email_valid == false)
-		{
-			$('#email_error').html('That email address is invalid').show('slow');
-			$('#email_error').delay(2500).hide('slow');		
-		}
+				$.ajax(
+				{
+					url			: base_url + 'api/users/password_forgot',
+					type		: 'POST',
+					dataType	: 'json',
+					data		: $('#forgot_password').serializeArray(),
+			  		success		: function(result)
+			  		{					
+						if (result.status == 'success')
+						{
+							$('#content_message').notify({status:result.status,message:result.message,complete:'redirect',redirect: base_url + 'login'});					
+						}
+						else
+						{
+							$('#content_message').notify({status:result.status,message:result.message});					
+						}
+				 	}
+				});
+			}
+		});
 	});
 });
 </script>
