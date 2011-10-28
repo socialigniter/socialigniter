3f6af2f65f709df600290f1233bde02987bff922
Brennan Novak <brennan@reverseproductions.com>
2011-10-28 12:15:42 -0700

    pushing up latest

--- install.php
+++ install.php
@@ -1,32 +1,33 @@
-<?PHP if ($_POST["hostname"]) {
-
-$config_file = "./application/config/config.php.TEMPLATE";
-$current = file_get_contents($config_file, FILE_USE_INCLUDE_PATH);
-$current = str_replace("http://localhost/", $_POST["base_site"], $current);
-file_put_contents("./application/config/config.php", $current);
-
-$hostname = $_POST["hostname"];
-$username= $_POST["username"];
-$password= $_POST["password"];
-$database_name = $_POST["database_name"];
-
-$database_file = file_get_contents("./application/config/database.php.TEMPLATE", FILE_USE_INCLUDE_PATH);
-$database_file = str_replace("['dev']['hostname'] = 'localhost'", "['dev']['hostname'] = '$hostname'", $database_file);
-$database_file = str_replace("['dev']['username'] = 'root'", "['dev']['username'] = '$username'", $database_file);
-$database_file = str_replace("['dev']['password'] = ''", "['dev']['password'] = '$password'", $database_file);
-$database_file = str_replace("['dev']['database'] = 'social-igniter'", "['dev']['database'] = '$database_name'", $database_file);
-
-file_put_contents("./application/config/database.php", $database_file);
-
-copy("./application/config/routes.php.TEMPLATE","./application/config/routes.php");
-copy("./application/config/custom.php.TEMPLATE","./application/config/custom.php");
-copy("./application/config/social_igniter.php.TEMPLATE","./application/config/social_igniter.php");
-copy("./application/helpers/custom_helper.php.TEMPLATE","./application/helpers/custom_helper.php");
-
-header("Location: /");
-
+<?PHP 
+if ($_POST["hostname"]) 
+{
+	$config_file = "./application/config/config.php.TEMPLATE";
+	$current = file_get_contents($config_file, FILE_USE_INCLUDE_PATH);
+	$current = str_replace("http://localhost/", $_POST["base_site"], $current);
+	file_put_contents("./application/config/config.php", $current);
+	
+	$hostname = $_POST["hostname"];
+	$username= $_POST["username"];
+	$password= $_POST["password"];
+	$database_name = $_POST["database_name"];
+	
+	$database_file = file_get_contents("./application/config/database.php.TEMPLATE", FILE_USE_INCLUDE_PATH);
+	$database_file = str_replace("['dev']['hostname'] = 'localhost'", "['dev']['hostname'] = '$hostname'", $database_file);
+	$database_file = str_replace("['dev']['username'] = 'root'", "['dev']['username'] = '$username'", $database_file);
+	$database_file = str_replace("['dev']['password'] = ''", "['dev']['password'] = '$password'", $database_file);
+	$database_file = str_replace("['dev']['database'] = 'social-igniter'", "['dev']['database'] = '$database_name'", $database_file);
+	
+	file_put_contents("./application/config/database.php", $database_file);
+	
+	copy("./application/config/routes.php.TEMPLATE","./application/config/routes.php");
+	copy("./application/config/custom.php.TEMPLATE","./application/config/custom.php");
+	copy("./application/config/social_igniter.php.TEMPLATE","./application/config/social_igniter.php");
+	copy("./application/helpers/custom_helper.php.TEMPLATE","./application/helpers/custom_helper.php");
+	
+	header("Location: /");
 }
-else{
+else
+{
 ?>
 <form method="POST">
 <h1> Base Site URL</h1>
@@ -46,5 +47,4 @@ The name of the database you want to connect to. <input type="text" placeholder=
 
 <input type="submit">
 </form>
-
 <?php } ?>
