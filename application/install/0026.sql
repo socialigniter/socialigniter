6b1b76e92a4c063dba86a654631898c177516d47
Brennan Novak <brennan@reverseproductions.com>
2011-12-14 14:36:24 -0800

    make sure all is committed

---
+++ application/install/0026.sql
@@ -0,0 +1,12 @@
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'site_logo', '');
+
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'header_image', 'left top');
+
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'background_image', '');
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'background_position', 'left top');
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'background_repeat', 'repeat-x');
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'background_color', 'ffffff');
+
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'font_color_normal', '0066CC');
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'font_color_visited', '0066CC');
+INSERT INTO `settings` VALUES(NULL, 1, 'design', 'font_color_hover', 'ff3300');
