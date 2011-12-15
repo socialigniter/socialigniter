85817f026678fb936dc47073671af5a2705b0804
Brennan Novak <brennan@reverseproductions.com>
2011-12-15 10:13:20 -0800

    made item_timeline not display edit if content_id is null

--- application/views/dashboard_default/partials/item_timeline.php
+++ application/views/dashboard_default/partials/item_timeline.php
@@ -19,8 +19,9 @@
 			<?php if ((config_item('home_like') == 'TRUE') && ($item_content_id)): ?>
 			<li><a href="#"><span class="actions action_like"></span> Like</a></li>
 			<?php endif; ?>
-			<?php if ($item_can_modify): ?>
+			<?php if ($item_can_modify): if ($item_content_id): ?>
 			<li><a class="item_edit" href="<?= $item_edit; ?>" id="item_action_edit_<?= $item_id ?>"><span class="actions action_edit"></span> Edit</a></li>
+			<?php endif; ?>
 			<li><a class="item_delete" href="<?= $item_delete; ?>" id="item_action_delete_<?= $item_id ?>"><span class="actions action_delete"></span> Delete</a></li>
 			<?php endif; ?>
 		</ul>
