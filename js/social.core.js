4f8e9c2b036a47f0031df47b7e9a9e1b1a7e049d
Brennan Novak <brennan@reverseproductions.com>
2012-02-02 13:16:57 -0800

    added null class to social.core.js so browsers without console.log() do not crash anymore

--- js/social.core.js
+++ js/social.core.js
@@ -1,3 +1,6 @@
+// If no console.log() exists
+if (!window.console) window.console = { log: $.noop, group: $.noop, groupEnd: $.noop, info: $.noop, error: $.noop };
+
 /* Auth & Crypto Functions 
 	- jquery.oauth.js
 	- sha1.js
