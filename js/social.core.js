99573cabc44ff4a19883614ee093eac5e44623d9
Brennan Novak <hi@brennannovak.com>
2012-08-16 17:45:40 -0700

    Added Credit Card Validation and Type Determine to social.core.js Ref #56

--- js/social.core.js
+++ js/social.core.js
@@ -697,6 +697,37 @@ function validateUsPhoneNumber(phone_number)
 	return phone_number_pattern.test(phone_number);  
 }
 
+function validateCreditCard(creditcard)
+{
+    if (getCreditCardTypeByNumber(creditcard) == '?') return false;
+    return true;
+}
+
+// Credit Card
+function getCreditCardTypeByNumber(ccnumber) {
+    var cc = (ccnumber + '').replace(/\s/g, ''); //remove space
+ 
+    if ((/^(34|37)/).test(cc) && cc.length == 15) {
+        return 'AMEX'; //AMEX begins with 34 or 37, and length is 15.
+    } else if ((/^(51|52|53|54|55)/).test(cc) && cc.length == 16) {
+        return 'MasterCard'; //MasterCard beigins with 51-55, and length is 16.
+    } else if ((/^(4)/).test(cc) && (cc.length == 13 || cc.length == 16)) {
+        return 'Visa'; //VISA begins with 4, and length is 13 or 16.
+    } else if ((/^(300|301|302|303|304|305|36|38)/).test(cc) && cc.length == 14) {
+        return 'DinersClub'; //Diners Club begins with 300-305 or 36 or 38, and length is 14.
+    } else if ((/^(2014|2149)/).test(cc) && cc.length == 15) {
+        return 'enRoute'; //enRoute begins with 2014 or 2149, and length is 15.
+    } else if ((/^(6011)/).test(cc) && cc.length == 16) {
+        return 'Discover'; //Discover begins with 6011, and length is 16.
+    } else if ((/^(3)/).test(cc) && cc.length == 16) {
+        return 'JCB';  //JCB begins with 3, and length is 16.
+    } else if ((/^(2131|1800)/).test(cc) && cc.length == 15) {
+        return 'JCB';  //JCB begins with 2131 or 1800, and length is 15.
+    }
+    return '?';
+}
+
+
 // Checks if field has content, handles placeholder
 function isFieldValid(id, placeholder, error)
 {
@@ -1978,7 +2009,6 @@ $(function()
 			return confirm_state;
 		}
 
-
 		// Message Types
 		function messageLabel(valid, element)
 		{	
@@ -2053,6 +2083,10 @@ $(function()
 			else if (element.rule == 'confirm')
 			{
 				is_valid = validateConfirm(validate, element.selector);
+			}
+			else if (element.rule == 'credit_card')
+			{
+				is_valid = validateCreditCard(validate);
 			}
 			else if (jQuery.isFunction(element.rule))
 			{			
