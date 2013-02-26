2479f15497efd28667b50121e19146c5ab94b5bc
Brennan Novak <hi@brennannovak.com>
2013-02-26 15:51:55 -0800

    Refactored a bit of Site_controller.php

--- application/libraries/Site_Controller.php
+++ application/libraries/Site_Controller.php
@@ -1,4 +1,4 @@
-<?php  if  ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Site_Controller Library
  * 
@@ -14,8 +14,7 @@ class Site_Controller extends MY_Controller
         parent::__construct(); 
         
 		// Dashboard & Public values for logged
-		if ($this->social_auth->logged_in())
-		{
+		if ($this->social_auth->logged_in()):
 			// OAuth Tokens
 			$this->data['oauth_consumer_key'] 	= $this->session->userdata('consumer_key');
 			$this->data['oauth_consumer_secret']= $this->session->userdata('consumer_secret');
@@ -41,9 +40,7 @@ class Site_Controller extends MY_Controller
 			$this->data['link_profile']			= base_url()."people/".$this->session->userdata('username');
 			$this->data['link_settings']		= base_url()."settings/profile";
 			$this->data['link_logout']			= base_url().'logout';	
-		}
-		else
-		{
+		else:
 			// OAuth Tokens
 			$this->data['oauth_consumer_key'] 	= '';
 			$this->data['oauth_consumer_secret']= '';
@@ -63,98 +60,85 @@ class Site_Controller extends MY_Controller
 			$this->data['logged_location']		= '';
 			$this->data['logged_geo_enabled']	= '';
 			$this->data['logged_privacy']		= '';
-		}        
-            
+		endif;
 
-        // Load Basic Views
-        $head_view			= config_item('site_theme').'/partials/head_site';
-        $logged_view		= config_item('site_theme').'/partials/logged';
-		$footer_view		= config_item('site_theme').'/partials/footer_site';
 
-        if (file_exists(APPPATH.'views/'.$head_view.'.php'))
-        {
+        // Load Head
+        $head_view = config_item('site_theme').'/partials/head_site';
+        if (file_exists(APPPATH.'views/'.$head_view.'.php')):
         	$this->data['head']		= $this->load->view($head_view, $this->data, true);
-        }
-        else
-        {
+        else:
         	$this->data['head']		= '';
-        }
+        endif;
 
-        if (file_exists(APPPATH.'views/'.$logged_view.'.php'))
-        {
+
+        // Load Logged
+        $logged_view = config_item('site_theme').'/partials/logged';
+        if (file_exists(APPPATH.'views/'.$logged_view.'.php')):
         	$this->data['logged']	= $this->load->view($logged_view, $this->data, true);
-        }
-        else
-        {
+        else:
         	$this->data['logged']	= '';
-        }
-        
+        endif;
+
+      
         // Set Empty Values
         $this->data['site_image']	= '';
         $this->data['content']		= '';
 
  		// Widget Regions	 		
- 		foreach ($this->site_theme->layouts as $key => $site_layout)
- 		{ 		
- 			if ($key == 'sidebar')
- 			{
- 				foreach ($site_layout as $region)
- 				{
+ 		foreach ($this->site_theme->layouts as $key => $site_layout):
+ 			if ($key == 'sidebar'):
+ 				foreach ($site_layout as $region):
 					$this->data[$region] = '';			
- 				} 			
- 			}
- 		}
+ 				endforeach;			
+ 			endif;
+ 		endforeach;
+
 
 		// Load Views
         $this->data['shared_ajax']		= '';        
 		$this->data['comments_view'] 	= '';
 
-		// Load Footer
-        if (file_exists(APPPATH.'views/'.$footer_view.'.php'))
-        {
-        	$this->data['footer']	= $this->load->view($footer_view, $this->data, true);
-        }
-        else
-        {
-        	$this->data['footer']	= '';
-        }
 
-		// If Modules Exist		
-		if ($this->modules_scan)
-		{
-			foreach ($this->modules_scan as $module)
-			{
-				if (config_item($module.'_enabled') == 'TRUE')
-				{
+		// Load Footer
+		$footer_view = config_item('site_theme').'/partials/footer_site';
+        if (file_exists(APPPATH.'views/'.$footer_view.'.php')):
+        	$this->data['footer']	= $this->load->view('/'.$footer_view.'.php', $this->data, true);
+        else:
+        	$this->data['footer']	= '';
+        endif;
+
+		// If Modules Exist
+		if ($this->modules_scan):
+			foreach ($this->modules_scan as $module):
+				if (config_item($module.'_enabled') == 'TRUE'):
 					$module_head 	= 'modules/'.$module.'/views/partials/head_site.php';
 					$module_footer 	= 'modules/'.$module.'/views/partials/footer_site.php';
 
 					// Set Module Asset Path
 					$this->data['this_module_assets'] 	= base_url().'application/modules/'.$module.'/assets/';
-				
-					// Include Heads
-					if ($this->site_theme->head_footer == 'yes')
-					{
-					    if (file_exists(APPPATH.$module_head))
-					    {
+
+					// If Enabled
+					if ($this->site_theme->head_footer == 'yes'):
+						// Include Heads
+					    if (file_exists(APPPATH.$module_head)):
 					    	$this->data['head'] .= $this->load->view('../'.$module_head, $this->data, true);
-					    }
+					    endif;
 						
 						// Include Footers
-					    if (file_exists(APPPATH.$module_footer))
-					    {
+					    if (file_exists(APPPATH.$module_footer)):
 					    	$this->data['footer'] .= $this->load->view('../'.$module_footer, $this->data, true);
-						}
-					}
-				}
-			}
-		}
+						endif;
+					endif;
+				endif;
+			endforeach;
+		endif;
+
 
 		// This Module Assets
-		if ($this->module_name)
-		{
+		if ($this->module_name):
 			$this->data['this_module_assets'] = base_url().'application/modules/'.$this->module_name.'/assets/';
-		}
+		endif;
     }
 
 	/**
@@ -170,84 +154,79 @@ class Site_Controller extends MY_Controller
     	if (!$content)	$content = $this->action_name;	
 
  		// Do Widgets
-		foreach ($this->site_theme->layouts->$layout as $region)
-		{
+		foreach ($this->site_theme->layouts->$layout as $region):
 			$this->data[$region] = $this->render_widgets($region, $layout);	
-		}
-		
+		endforeach;
+
 
       	// Is Module
-       	if ($this->module_name)
-    	{
+       	if ($this->module_name):
     	    $content_path = '../modules/'.$this->module_name.'/views/'.$this->module_controller.'/'.$content;    	    
-		}
-		else
-		{
+		else:
         	$content_path = config_item('site_theme').'/'.$this->controller_name.'/'.$content;
-		}
+		endif;
+
 
     	// Content file exists
-        if (file_exists(APPPATH.'views/'.$content_path.'.php'))
-        {
+        if (file_exists(APPPATH.'views/'.$content_path.'.php')):
             $this->data['content'] .= $this->load->view($content_path, $this->data, true);
-        }
-        else
-        {
+        else:
         	$this->data['content'] .= 'Oops that content file is mising';
-        }
- 		 		
+        endif;
+
+	
 		// Render View
         $this->load->view(config_item('site_theme').'/layouts/'.$layout, $this->data);
     }
 
+	/**
+	 * render_custom_layout() – Renders a custom view 
+	 * 
+	 * @param string $layout The full application path to the layout '../../../modules/app_name/views/layouts/custom'
+	 */
     function render_custom_layout($layout)
     {
         $this->load->view($layout, $this->data);
     }
-   
-    // Renders Widgets
+
+	/**
+	 * render_widgets() – Renders widgets on a page
+	 * 
+	 * @param string $layout The full application path to the layout '../../../modules/app_name/views/layouts/custom'
+	 */
     function render_widgets($region, $layout=NULL)
     {
     	$widgets = '';
     	$site_widgets = $this->social_igniter->make_widgets_order($this->site_widgets, $layout);
     	    	   
    		// Loop Through All Widgets
-    	foreach ($site_widgets as $site_widget)
-    	{
+    	foreach ($site_widgets as $site_widget):
     		$widget = json_decode($site_widget->value);
     		
     		// If Region & Layout Are Correct
-    		if ($site_widget->setting == $region AND $widget->layout == $layout)
-    		{
+    		if ($site_widget->setting == $region AND $widget->layout == $layout):
     			$this->data['widget_id']		= $site_widget->settings_id;
     			$this->data['widget_region']	= $region;
 				$this->data['widget_title']		= $widget->title;
  				$this->data['widget_content'] 	= $widget->content;
 
 				// View or Run Widget
-    			if ($widget->method == 'view')
- 				{
+    			if ($widget->method == 'view'):
     				$widgets .= $this->load->view('widgets/'.$widget->path, $this->data, true);
-    			}
-    			elseif ($widget->method == 'run')
-    			{
+    			elseif ($widget->method == 'run'):
     				// Is Core Widget
-    				if (in_array($widget->module, config_item('core_modules')))
-    				{
+    				if (in_array($widget->module, config_item('core_modules'))):
     					$widgets .= modules::run('site/'.$widget->path, $this->data);    					
-    				}
-    				else
-    				{
+    				else:
     					$widgets .= modules::run($widget->module.'/'.$widget->path, $this->data);
-    				}
-    			}
-				else
-				{
+    				endif;
+				else:
 					$widgets .= '';
-				}
-    		}
-    	}
+				endif;
+    		endif;
+    	endforeach;
 
 		return $widgets;
     }
+
 }
